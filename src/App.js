6dfb2b4fc4768d13f03dc291b859b30f44f43e92
Cheryl <cheryl.nqj@gmail.com>
2020-10-13 16:14:44 +0800

    Set up frontend navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import EventCards from './views/EventCards'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <EventCards />
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { AppBar, Toolbar, Typography, Tabs, Tab, Link } from '@material-ui/core'
+// import { Link } from "react-router-dom";
+
+class NavBar extends Component {
+    render() {
+        var styles = {
+            appBar: {
+                flexWrap: 'wrap'
+            },
+            tabs: {
+                width: '100%'
+            }
+        };
+        var allTabs = ['/events', '/create']
+
+        return (
+                <div>
+                    <AppBar position="sticky">
+                        <Toolbar>
+                            <Typography variant="title" color="inherit">
+                                    University Supper Club
+                            </Typography>
+                            <Tabs>
+                                <Tab label="See Events" value="/events" component={Link} to={allTabs[0]} />
+                                <Tab label="Create Events" value="/create" component={Link} to={allTabs[1]} />
+                            </Tabs>
+                        </Toolbar>
+                    </AppBar>
+                </div>
+        );
+    }
+}
+
+export default NavBar;
